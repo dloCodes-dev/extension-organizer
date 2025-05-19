4f7330c5156dde620cc58fa8eec1b115fa416df5
Darren Lo <dlo.frontenddev@gmail.com>
2025-05-19 14:33:30 -0400

    Added inactive feature to the card

--- script/script.js
+++ script/script.js
@@ -66,10 +66,9 @@ const darkMode = (extensions) => {
 	header.classList.toggle('extension__header--dark');
 	nav.classList.toggle('extension__nav--dark');
 	document.body.classList.toggle('dark-mode');
+	console.log('clicked');
 };
 
-//* 1.) I need to somehow switch the flag within the object from false to true or vice versa everytime the toggle goes off
-//* 2.) Afterwards need to retain all the nodes that have the --inactive / --active flags and loop through them to turn the flag on / off
 const toggleExtension = (evt, allExtensions) => {
 	const toggleSwitch = evt.target;
 	const currentHeader = toggleSwitch
@@ -95,8 +94,95 @@ const changeActiveStatus = (headerContent, allExtensions, flag) => {
 			eachExtension.isActive = flag;
 		}
 	});
+};
 
-	console.log(allExtensions);
+const activeExtensions = (allExtensions) => {
+	const cardContainer = document.querySelector('.extensions-container');
+
+	while (cardContainer.firstChild) {
+		cardContainer.removeChild(cardContainer.lastChild);
+	}
+
+	allExtensions.forEach((eachExtension) => {
+		if (eachExtension.isActive === true) {
+			const extensionCard = document.createElement('div');
+			const cardHeaderContainer = document.createElement('div');
+			const textContentContainer = document.createElement('div');
+			const extensionImage = document.createElement('img');
+			const extensionTitle = document.createElement('h3');
+			const extensionContent = document.createElement('p');
+			const removeToggleContainer = document.createElement('div');
+			const removeButton = document.createElement('div');
+			const toggleContainer = document.createElement('div');
+			const toggleActive = document.createElement('div');
+
+			extensionImage.setAttribute('src', `${eachExtension.logo}`);
+			extensionTitle.textContent = `${eachExtension.name}`;
+			extensionContent.textContent = `${eachExtension.description}`;
+			removeButton.textContent = 'Remove';
+
+			extensionCard.classList.add('extensions__card');
+			cardHeaderContainer.classList.add('extensions__header');
+			textContentContainer.classList.add('extensions__header-content');
+			extensionTitle.classList.add('extensions__title');
+			removeToggleContainer.classList.add('extensions__toggle-container');
+			removeButton.classList.add('extensions__remove');
+			toggleContainer.classList.add('extensions__toggle');
+			toggleActive.classList.add('extensions__toggle--active');
+
+			cardContainer.appendChild(extensionCard);
+			extensionCard.append(cardHeaderContainer, removeToggleContainer);
+			cardHeaderContainer.append(extensionImage, textContentContainer);
+			textContentContainer.append(extensionTitle, extensionContent);
+			removeToggleContainer.append(removeButton, toggleContainer);
+			toggleContainer.appendChild(toggleActive);
+		}
+	});
+};
+
+const inactiveExtensions = (allExtensions) => {
+	const cardContainer = document.querySelector('.extensions-container');
+
+	while (cardContainer.firstChild) {
+		cardContainer.removeChild(cardContainer.lastChild);
+	}
+
+	allExtensions.forEach((eachExtension) => {
+		console.log(eachExtension);
+		if (eachExtension.isActive === false) {
+			const extensionCard = document.createElement('div');
+			const cardHeaderContainer = document.createElement('div');
+			const textContentContainer = document.createElement('div');
+			const extensionImage = document.createElement('img');
+			const extensionTitle = document.createElement('h3');
+			const extensionContent = document.createElement('p');
+			const removeToggleContainer = document.createElement('div');
+			const removeButton = document.createElement('div');
+			const toggleContainer = document.createElement('div');
+			const toggleActive = document.createElement('div');
+
+			extensionImage.setAttribute('src', `${eachExtension.logo}`);
+			extensionTitle.textContent = `${eachExtension.name}`;
+			extensionContent.textContent = `${eachExtension.description}`;
+			removeButton.textContent = 'Remove';
+
+			extensionCard.classList.add('extensions__card');
+			cardHeaderContainer.classList.add('extensions__header');
+			textContentContainer.classList.add('extensions__header-content');
+			extensionTitle.classList.add('extensions__title');
+			removeToggleContainer.classList.add('extensions__toggle-container');
+			removeButton.classList.add('extensions__remove');
+			toggleContainer.classList.add('extensions__toggle');
+			toggleActive.classList.add('extensions__toggle--active');
+
+			cardContainer.appendChild(extensionCard);
+			extensionCard.append(cardHeaderContainer, removeToggleContainer);
+			cardHeaderContainer.append(extensionImage, textContentContainer);
+			textContentContainer.append(extensionTitle, extensionContent);
+			removeToggleContainer.append(removeButton, toggleContainer);
+			toggleContainer.appendChild(toggleActive);
+		}
+	});
 };
 
 const dynamicElements = getBookmarkData();
@@ -104,14 +190,30 @@ const dynamicElements = getBookmarkData();
 dynamicElements.then((results) => {
 	const [extensionCards, jsonOBJ] = results;
 
-	const darkModeButton = document.querySelector('.extension__nav--moon');
 	const containerForCards = document.querySelector('.extensions-container');
-	// const active = document.querySelector('')
+	const allButton = document.querySelector('.extension__header > ul > li:first-child');
+	const activeButton = document.querySelector('.extension__header > ul > li:nth-child(2)');
+	const inactiveButton = document.querySelector('.extension__header > ul > li:last-child');
 
-	darkModeButton.addEventListener('click', () => {
-		darkMode(extensionCards);
-	});
 	containerForCards.addEventListener('click', (evt) => {
 		toggleExtension(evt, jsonOBJ);
+	});
+
+	allButton.addEventListener('click', () => {
+		const cardContainer = document.querySelector('.extensions-container');
+
+		while (cardContainer.firstChild) {
+			cardContainer.removeChild(cardContainer.lastChild);
+		}
+
+		getBookmarkData();
+	});
+
+	activeButton.addEventListener('click', () => {
+		const flag = activeExtensions(jsonOBJ);
+	});
+
+	inactiveButton.addEventListener('click', () => {
+		inactiveExtensions(jsonOBJ);
 	});
 });
