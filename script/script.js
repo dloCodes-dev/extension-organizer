1153f0b3932c4bd5ff19b86eb582cbf46c092a2d
Darren Lo <dlo.frontenddev@gmail.com>
2025-05-19 16:00:05 -0400

    Fixed logic to active and inactive. Added logic for All nav

--- script/script.js
+++ script/script.js
@@ -1,14 +1,14 @@
-const getBookmarkData = async () => {
+const getExtensionData = async () => {
 	try {
 		const extensionJSON = await fetch('../data.json');
 		const extensionData = await extensionJSON.json();
-		return displayBookmarks(extensionData);
+		return extensionData;
 	} catch (error) {
 		console.error(error);
 	}
 };
 
-const displayBookmarks = (allExtensions) => {
+const displayExtensions = (allExtensions) => {
 	const cardContainer = document.querySelector('.extensions-container');
 
 	allExtensions.forEach((extension, i) => {
@@ -96,6 +96,51 @@ const changeActiveStatus = (headerContent, allExtensions, flag) => {
 	});
 };
 
+const showExtensions = (allExtensions) => {
+	const cardContainer = document.querySelector('.extensions-container');
+
+	console.log(allExtensions);
+
+	allExtensions.forEach((extension) => {
+		const extensionCard = document.createElement('div');
+		const cardHeaderContainer = document.createElement('div');
+		const textContentContainer = document.createElement('div');
+		const extensionImage = document.createElement('img');
+		const extensionTitle = document.createElement('h3');
+		const extensionContent = document.createElement('p');
+		const removeToggleContainer = document.createElement('div');
+		const removeButton = document.createElement('div');
+		const toggleContainer = document.createElement('div');
+		const toggleActive = document.createElement('div');
+
+		extensionCard.classList.add('extensions__card');
+		cardHeaderContainer.classList.add('extensions__header');
+		textContentContainer.classList.add('extensions__header-content');
+		extensionTitle.classList.add('extensions__title');
+		removeToggleContainer.classList.add('extensions__toggle-container');
+		removeButton.classList.add('extensions__remove');
+		toggleContainer.classList.add('extensions__toggle');
+
+		if (extension.isActive === false) {
+			toggleActive.classList.add('extensions__toggle--inactive');
+		} else {
+			toggleActive.classList.add('extensions__toggle--active');
+		}
+
+		extensionImage.setAttribute('src', `${extension.logo}`);
+		extensionTitle.textContent = `${extension.name}`;
+		extensionContent.textContent = `${extension.description}`;
+		removeButton.textContent = 'Remove';
+
+		cardContainer.appendChild(extensionCard);
+		extensionCard.append(cardHeaderContainer, removeToggleContainer);
+		cardHeaderContainer.append(extensionImage, textContentContainer);
+		textContentContainer.append(extensionTitle, extensionContent);
+		removeToggleContainer.append(removeButton, toggleContainer);
+		toggleContainer.appendChild(toggleActive);
+	});
+};
+
 const activeExtensions = (allExtensions) => {
 	const cardContainer = document.querySelector('.extensions-container');
 
@@ -148,7 +193,6 @@ const inactiveExtensions = (allExtensions) => {
 	}
 
 	allExtensions.forEach((eachExtension) => {
-		console.log(eachExtension);
 		if (eachExtension.isActive === false) {
 			const extensionCard = document.createElement('div');
 			const cardHeaderContainer = document.createElement('div');
@@ -173,7 +217,7 @@ const inactiveExtensions = (allExtensions) => {
 			removeToggleContainer.classList.add('extensions__toggle-container');
 			removeButton.classList.add('extensions__remove');
 			toggleContainer.classList.add('extensions__toggle');
-			toggleActive.classList.add('extensions__toggle--active');
+			toggleActive.classList.add('extensions__toggle--inactive');
 
 			cardContainer.appendChild(extensionCard);
 			extensionCard.append(cardHeaderContainer, removeToggleContainer);
@@ -185,12 +229,11 @@ const inactiveExtensions = (allExtensions) => {
 	});
 };
 
-const dynamicElements = getBookmarkData();
-
-dynamicElements.then((results) => {
-	const [extensionCards, jsonOBJ] = results;
+getExtensionData().then((jsonOBJ) => {
+	displayExtensions(jsonOBJ);
 
 	const containerForCards = document.querySelector('.extensions-container');
+
 	const allButton = document.querySelector('.extension__header > ul > li:first-child');
 	const activeButton = document.querySelector('.extension__header > ul > li:nth-child(2)');
 	const inactiveButton = document.querySelector('.extension__header > ul > li:last-child');
@@ -206,7 +249,8 @@ dynamicElements.then((results) => {
 			cardContainer.removeChild(cardContainer.lastChild);
 		}
 
-		getBookmarkData();
+		//! need to add function to display all extensions
+		showExtensions(jsonOBJ);
 	});
 
 	activeButton.addEventListener('click', () => {
